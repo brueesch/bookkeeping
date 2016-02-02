53509689557b9c8e27b5ff4c7e3b2025b4f3e5d9
Christian <c.brueesch@yahoo.de>
2016-02-02 01:27:00 +0100

    Extended EntryCategory

--- src/ch/brueesch/model/EntryCategory.java
+++ src/ch/brueesch/model/EntryCategory.java
@@ -7,7 +7,23 @@ public enum EntryCategory {
 
     SALARY("Salary"),
     EXPENSES("Expenses"),
-    OTHER_INCOME("Other Income");
+    OTHER_INCOME("Other Income"),
+    TAX("Taxes"),
+    RENT("Rent"),
+    INSURANCE("Insurance"),
+    ELECTRIC("Electric"),
+    MOBILE_PHONE("Mobile Phone"),
+    CABLECOM("Cablecom"),
+    SBB("SBB"),
+    DONATION("Donation"),
+    SPOTIFY("Spotify"),
+    CLEANING("Cleaning"),
+    FOOD("Food"),
+    CLOTH("Cloth"),
+    LEISURE("Leisure"),
+    VACATION("Vacation"),
+    RESERVE("Reserve"),
+    BACK_PAYMENT("Back Payment");
 
 
 
